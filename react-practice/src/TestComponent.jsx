2db34888f7488fb3071d4068017ff208f2b55483
Joinal Abden <joinalzx@gmail.com>
2026-09-08 23:38:00 +0600

    react work

---
+++ react-practice/src/TestComponent.jsx
@@ -0,0 +1,12 @@
+//  1. File extension must have .jsx
+//  2. there must be a single root return
+
+// const TestComponent = () => {
+//   return <h1>Test Component</h1>;
+// };
+
+// export default TestComponent;
+
+export default function TestComponent() {
+  return <h1> First Component</h1>;
+}
